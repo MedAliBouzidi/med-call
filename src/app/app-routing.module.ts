4d9696ed05e45dc13673d5d7faa586edb8d316e8
BOUZIDI Med Ali <med23908@gmail.com>
2021-09-02 11:46:53 +0100

    Add: Forget Password Component and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./components/security/login/login.component";
 import { RegisterComponent } from "./components/security/register/register.component";
+import { ForgetPasswordComponent } from "./components/security/forget-password/forget-password.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
