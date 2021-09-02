d27ee0d477fb1356b554df8b2536f0980e187844
BOUZIDI Med Ali <med23908@gmail.com>
2021-09-02 11:20:45 +0100

    Update: Add routerLink to Login and Register pages

--- src/app/components/security/login/login.component.ts
+++ src/app/components/security/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
 @Component({
@@ -8,30 +7,14 @@ import { Router } from "@angular/router";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-
-  userNotFound: boolean = false;
 
   constructor(
     private router: Router
   ) {
-    this.loginForm = new FormGroup({
-      'email': new FormControl(null, [
-        Validators.required,
-        Validators.email
-      ]),
-      'password': new FormControl(null, [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.maxLength(15)
-      ])
-    });
+
   }
 
   ngOnInit(): void {
   }
 
-  logIn(lForm: FormGroupDirective) {
-
-  }
 }
