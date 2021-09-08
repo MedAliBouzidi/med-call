749a509c4e21218122bf09efa160d8eea7146748
BOUZIDI Med Ali <med23908@gmail.com>
2021-09-08 09:20:24 +0100

    Update: make register form reactive

--- src/app/components/security/register/register.component.ts
+++ src/app/components/security/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, FormGroupDirective, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,11 +8,63 @@ import { Router } from "@angular/router";
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(
-    private router: Router
-  ) { }
+  registerForm: FormGroup;
+  userRole = ['PATIENT', 'PRO_SANTE', 'ADMIN']
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.pattern("^[a-z][a-z0-9_]{7,29}$")
+      ]],
+      fullName: ['', [
+        Validators.required,
+        Validators.pattern("^[A-Za-z]+([\\ A-Za-z]+)*")
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.pattern("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(15)
+      ]],
+      confirmPassword: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(15)
+      ]],
+      address: ['', Validators.required],
+      phone: ['+216', [
+        Validators.required,
+        Validators.pattern("((\\+|00)216)[0-9]{8}")
+      ]],
+      speciality: ['', Validators.required],
+      cin: ['', [
+        Validators.required,
+        Validators.pattern("^(0|1)[0-9]{7}$")
+      ]],
+      userRole: ['', Validators.required]
+    });
   }
 
+  ngOnInit(): void {
+
+  }
+
+  onRegister() {
+    console.log(this.registerForm.value['username'])
+    console.log(this.registerForm.value['fullName'])
+    console.log(this.registerForm.value['email'])
+    console.log(this.registerForm.value['password'])
+    console.log(this.registerForm.value['confirmPassword'])
+    console.log(this.registerForm.value['address'])
+    console.log(this.registerForm.value['phone'])
+    console.log(this.registerForm.value['speciality'])
+    console.log(this.registerForm.value['cin'])
+    console.log(this.registerForm.value['userRole'])
+  }
 }
